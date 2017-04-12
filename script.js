0f0a8038bd9035977c69a59d0176679ac035b61a
Alexey Kott <alexey.kott@gmail.com>
2017-04-12 17:43:04 +0300

    поправил скачивание репорта

--- script.js
+++ script.js
@@ -64,6 +64,9 @@ $("#logout").on("click", function(){
 
 
 $("#find-mutual-friends-users").on("click", function() {
+	$("#result").text("0");
+	counter = 0;
+	report = "";
 	if(!isLogin){
 		$("#login").trigger("click");
 		return false;
@@ -208,8 +211,7 @@ function endProcess(){
 	nMutualFriends = [];
 	userFriends = [];
 	$("#load").remove();
-	counter = 0;
-	report = "";
+	
 }
 
 var l = 0;
@@ -261,7 +263,7 @@ function addFriend(friend){
 					</div>
 				</a>`);
 			$("#result").text(++counter)
-			report+="https://vk.com/"+user.screen_name+"\n";
+			report+="https://vk.com/"+user.screen_name+"\n\r";
 		}
 		
 	})
